629070d38363ec2a797175a7316a12a1aa91d1a1
preddybreddy <p7.ready@gmail.com>
2023-09-22 16:24:56 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Button.css'
+import { Link } from 'react-router-dom'
+
+
+function Button({children, type, onClick, buttonStyle, buttonSize})
+{
+    const STYLES = ['btn--primary', 'btn--outline', 'btn--test']
+    const SIZES = ['btn--medium', 'btn--large']
+
+    return (
+        <>
+            <Link to='/sign-up' className='btn-mobile'>
+                <button className={`btn ${STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]} ${SIZES.includes(buttonSize) ? buttonSize : SIZES[0]}`} onClick={onClick} type={type}>
+                    {children}
+                </button>
+            </Link>
+        </>            
+    )
+}
+
+export default Button
